eaa8a07664a16c7428650fceebd968c41ce5209f
c <no-email@1.com>
2020-01-03 21:08:13 +0800

    payroll & payroll_test

---
+++ payroll_test/payroll_test.cpp
@@ -0,0 +1,11 @@
+// payroll_test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
